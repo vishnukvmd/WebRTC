b8efbfa223b227c1dcfc3b000446b1885d80aed3
Vishnu Mohandas <vishnumohandas@gmail.com>
2012-05-28 21:44:39 +0530

    Server now implemented using node.js and Socket.IO

---
+++ nodeServer.js
@@ -0,0 +1,42 @@
+// note, io.listen(<port>) will create a http server for you
+var io = require('socket.io').listen(8080);
+var nick = "";
+var socketList = [];
+var counter = 0;
+var userList = [];
+
+io.sockets.on('connection', function (socket) {
+
+	socket.on('signIn', function (name) {
+		for(var i=0;i<counter;i++) {
+			if(userList[i] == name) {
+				socket.emit('error', "Username already taken!");
+				return;
+			}
+		}
+		userList[counter] = name;		
+		console.log("User Added : "+name);
+		socket.emit('ready', counter, userList);
+		for(var i=0;i<counter;i++)
+			socketList[i].emit('addPeer', i, userList);		
+		socketList[counter] = socket;
+		counter++;
+		console.log("Users added!");
+	});
+	
+	socket.on('sendMessage', function (from, to, content) {
+		console.log(content);
+		socketList[to].emit('receiveMessage',from, content);
+		console.log("Message Sent!");
+	});
+	
+	socket.on('reject', function (myId, remoteId) {
+		socketList[remoteId].send(from, 'REJECT', userList[from]);
+	});
+	
+	socket.on('disconnect', function () {
+		for(var i=0;i<socketList.length;i++)
+			if(socket==socketList[i]) { socketList[i]=-1; console.log(i+"th User Deleted!"); break; }
+	});
+
+});
